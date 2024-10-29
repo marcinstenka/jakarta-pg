a4d12f1a584fa0cea8497cbc60d20a2dc1c012f3
marcinstenka <mastenka@wp.pl>
2024-10-29 22:46:43 +0100

    contollers interaces

--- heroes/src/main/java/stenka/marcin/heroes/fraction/controller/api/FractionController.java
+++ heroes/src/main/java/stenka/marcin/heroes/fraction/controller/api/FractionController.java
@@ -1,5 +1,17 @@
 package stenka.marcin.heroes.fraction.controller.api;
 
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.DELETE;
+import jakarta.ws.rs.FormParam;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.PATCH;
+import jakarta.ws.rs.PUT;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
 import stenka.marcin.heroes.fraction.dto.GetFractionResponse;
 import stenka.marcin.heroes.fraction.dto.GetFractionsResponse;
 import stenka.marcin.heroes.fraction.dto.PatchFractionRequest;
@@ -7,14 +19,29 @@ import stenka.marcin.heroes.fraction.dto.PutFractionRequest;
 
 import java.util.UUID;
 
+@Path("")
 public interface FractionController {
-    GetFractionResponse getFraction(UUID id);
+    @GET
+    @Path("/fractions/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    GetFractionResponse getFraction(@PathParam("id") UUID id);
 
+    @GET
+    @Path("/fractions")
+    @Produces(MediaType.APPLICATION_JSON)
     GetFractionsResponse getFractions();
 
-    void putFraction(UUID id, PutFractionRequest request);
+    @PUT
+    @Path("/fractions/{id}")
+    @Consumes({MediaType.APPLICATION_JSON})
+    void putFraction(@PathParam("id") UUID id, PutFractionRequest request);
 
-    void patchFraction(UUID id, PatchFractionRequest request);
+    @PATCH
+    @Path("/fractions/{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    void patchFraction(@PathParam("id") UUID id, PatchFractionRequest request);
 
-    void deleteFraction(UUID id);
+    @DELETE
+    @Path("/fractions/{id}")
+    void deleteFraction(@PathParam("id") UUID id);
 }
--- heroes/src/main/java/stenka/marcin/heroes/unit/controller/api/UnitController.java
+++ heroes/src/main/java/stenka/marcin/heroes/unit/controller/api/UnitController.java
@@ -1,5 +1,7 @@
 package stenka.marcin.heroes.unit.controller.api;
 
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
 import stenka.marcin.heroes.unit.dto.GetUnitResponse;
 import stenka.marcin.heroes.unit.dto.GetUnitsResponse;
 import stenka.marcin.heroes.unit.dto.PatchUnitRequest;
@@ -8,18 +10,36 @@ import stenka.marcin.heroes.unit.dto.PutUnitRequest;
 import java.util.UUID;
 
 public interface UnitController {
-    GetUnitsResponse getUserUnits(UUID id);
+    @GET
+    @Path("/users/{id}/units")
+    @Produces(MediaType.APPLICATION_JSON)
+    GetUnitsResponse getUserUnits(@PathParam("id") UUID id);
 
-    GetUnitsResponse getFractionUnits(UUID id);
+    @GET
+    @Path("/fractions/{id}/units")
+    @Produces(MediaType.APPLICATION_JSON)
+    GetUnitsResponse getFractionUnits(@PathParam("id") UUID id);
 
+    @GET
+    @Path("/units")
+    @Produces(MediaType.APPLICATION_JSON)
     GetUnitsResponse getUnits();
 
-    GetUnitResponse getUnit(UUID id);
+    @GET
+    @Path("/units/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    GetUnitResponse getUnit(@PathParam("id") UUID id);
 
-    void putUnit(UUID id, PutUnitRequest request);
+    @PUT
+    @Path("/units/{id}")
+    void putUnit(@PathParam("id") UUID id, PutUnitRequest request);
 
-    void patchUnit(UUID id, PatchUnitRequest request);
+    @PATCH
+    @Path("/units/{id}")
+    void patchUnit(@PathParam("id") UUID id, PatchUnitRequest request);
 
-    void deleteUnit(UUID id);
+    @DELETE
+    @Path("/units/{id}")
+    void deleteUnit(@PathParam("id") UUID id);
 
 }
